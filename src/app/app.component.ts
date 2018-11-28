de6c6eff5427b6f14cb5477826c8b71e75b5bc29
student <peter.gursky@upjs.sk>
2018-11-28 08:27:53 +0100

    bootstrap dotiahnuty do projektu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'registracia-itat-angular';
+  private title : string = 'Registrácia konferencie ITAT';
 }
